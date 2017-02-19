6b1b8bc975e1ec7c3c1dd82d16479cb387a30f39
Prayash Thapa <prayasht@gmail.com>
2017-02-19 04:44:25 -0700

    MapKit stuff finally functioning

--- iOS/informu/TagViewController.swift
+++ iOS/informu/TagViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class TagViewController: UIViewController {
+class TagViewController: UIViewController, MKMapViewDelegate {
     
     var locationManager: CLLocationManager!
     var mapView: MKMapView!
     var tag: Tag?
+    var destinationLocation: CLLocationCoordinate2D!
     
     override func viewDidLoad() {
         view.backgroundColor = .white
@@ -25,10 +26,12 @@ class TagViewController: UIViewController {
         let tagSettingsButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(TagViewController.showSettings))
         tagSettingsButton.tintColor = muOrange
         navigationItem.rightBarButtonItems = [tagSettingsButton]
+        setupToolbar()
     }
     
     func createMapView() {
         mapView = MKMapView()
+        mapView.delegate = self
         
         let leftMargin: CGFloat = 10
         let topMargin: CGFloat = 60
@@ -43,17 +46,90 @@ class TagViewController: UIViewController {
         // Show user location + zoom in
         mapView.showsUserLocation = true
         self.mapView.setUserTrackingMode(.follow, animated: false);
-        let userLocation = mapView.userLocation
-        print(userLocation)
+        
+        let sourceLocation = CLLocationCoordinate2D(latitude: 39.848903, longitude: -104.674398)
+        destinationLocation = CLLocationCoordinate2D(latitude: 39.847370, longitude: -104.680301)
+        
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+        
+        // 4.
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let sourceAnnotation = MKPointAnnotation()
+        sourceAnnotation.title = "You"
+        
+        if let location = sourcePlacemark.location {
+            sourceAnnotation.coordinate = location.coordinate
+        }
+        
+        let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.title = tag?.name
+        
+        if let location = destinationPlacemark.location {
+            destinationAnnotation.coordinate = location.coordinate
+        }
+        
+        mapView.showAnnotations([sourceAnnotation, destinationAnnotation], animated: true)
+        
+        // 7.
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .walking
+        
+        // Calculate the direction
+        let directions = MKDirections(request: directionRequest)
+        
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+            
+            let route = response.routes[0]
+            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            let boundingRectSize = MKMapSize(width: rect.size.width * 1.5, height: rect.size.height)
+            let boundingRectOrigin = MKMapPoint(x: rect.origin.x - 500, y: rect.origin.y)
+            let boundingRect = MKMapRect(origin: boundingRectOrigin, size: boundingRectSize)
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(boundingRect), animated: true)
+        }
         
         view.addSubview(mapView)
+        
+    }
+    
+    func openMaps() {
+        openMapForTag(lat: destinationLocation.latitude, long: destinationLocation.longitude)
     }
     
     func openMapForTag(lat: CLLocationDegrees, long: CLLocationDegrees) {
         let coordinate = CLLocationCoordinate2DMake(lat, long)
-        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary:nil))
+        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary: nil))
         mapItem.name = "Target location"
         mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving])
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = muOrange
+        renderer.lineWidth = 4.0
+        return renderer
+    }
+    
+    func setupToolbar() {
+        self.navigationController?.isToolbarHidden = false
+        let openMapsButton = UIBarButtonItem(title: "Open in Maps", style: .plain, target: self, action: #selector(openMaps))
+        openMapsButton.tintColor = muOrange
+        self.toolbarItems = [openMapsButton]
     }
     
     // 'lazy' = Only occurs once during the execution of the app
